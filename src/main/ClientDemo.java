1021743ec7a6a5a574a0c47a6044c04583976c06
jai-k-gohil <jai.k.gohil@gmail.com>
2019-11-21 23:57:08 +0530

    Refactoring

--- src/main/ClientDemo.java
+++ src/main/ClientDemo.java
@@ -16,7 +16,7 @@ public class ClientDemo {
     public static void main(String[] args) throws Exception{
         Client client = new Client();
        
-        if(client.initiateLogin("milind", "milind9819")){
+        if(client.initiateLogin("jai", "jai9819")){
             System.out.println("Login successful");
             List<File> files = FileManager.getFilesFrom(new File("C:\\Users\\Jai\\Desktop\\intrashare\\main\\libraries"));
             System.out.println(files);
